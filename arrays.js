f3f623b93be833eb7d1bfa01afc24909efdde2f3
Luciano Coelho de Souza <luciano.coelhodesouza@gmail.com>
2017-09-14 22:58:19 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -15,31 +15,31 @@ return  a.unshift(v);
 
 
 function addElementToEndOfArray(a,v){
-  return [...a, v]
+  return [...a, v];
 }
 
 
 function destructivelyAddElementToEndOfArray(a,v){
-  return a.push(v)
+  return a.push(v);
 }
 
 function accessElementInArray(a,i){
-  return a[i]
+  return a[i];
 }
 
 
 function destructivelyRemoveElementFromBeginningOfArray(a){
-  return a.shift()
+  return a.shift();
 }
 
 function removeElementFromBeginningOfArray(a){
-  return a.slice(1)
+  return a.slice(1);
 }
 
 function destructivelyRemoveElementFromEndOfArray(a){
-  return a.pop()
+  return a.pop();
 }
 
 function removeElementFromEndOfArray(a){
-  return a.slice(0,a.length()-1)
+  return a.slice(0,a.length()-1);
 }
