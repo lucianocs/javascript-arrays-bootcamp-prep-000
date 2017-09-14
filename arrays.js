503c89629cfcbe19c48f00b05331885609091e78
Luciano Coelho de Souza <luciano.coelhodesouza@gmail.com>
2017-09-14 22:56:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,45 +1,44 @@
-error here!!!
 
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray(a[],v){
-  var n = a;
-  return n.unshift[v]
+
+
+function addElementToBeginningOfArray(a,v){
+  return [v, ...a]
 }
 
 
-function destructivelyAddElementToBeginningOfArray(a[],v){
-  return a.unshift[v]
+function destructivelyAddElementToBeginningOfArray(a,v){
+  a.unshift(v)
 }
 
 
-function addElementToEndOfArray(a[],v){
-  var n = a;
-  return n.push[v]
+function addElementToEndOfArray(a,v){
+  return [...a, v]
 }
 
 
-function destructivelyAddElementToEndOfArray(a[],v){
-  return a.push[v]
+function destructivelyAddElementToEndOfArray(a,v){
+  return a.push(v)
 }
 
-function accessElementInArray(a[],i){
+function accessElementInArray(a,i){
   return a[i]
 }
 
 
-function destructivelyRemoveElementFromBeginningOfArray(a[]){
+function destructivelyRemoveElementFromBeginningOfArray(a){
   return a.shift()
 }
 
-function removeElementFromBeginningOfArray(a[]){
+function removeElementFromBeginningOfArray(a){
   return a.slice(1)
 }
 
-function destructivelyRemoveElementFromEndOfArray(a[]){
+function destructivelyRemoveElementFromEndOfArray(a){
   return a.pop()
 }
 
-function removeElementFromEndOfArray(a[]){
+function removeElementFromEndOfArray(a){
   return a.slice(0,a.length()-1)
 }
